eec70a0f81ca87ffb729a1062a453466aad26cbd
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-07-29 18:29:29 -0500

    Add photos to each instrument object

--- instruments-data.js
+++ instruments-data.js
@@ -2,16 +2,19 @@ const instruments = [
     {
         name: 'Hammond B3 Organ',
         family: 'keyboard',
-        components: ['keys', 'drawbars', 'pedals', 'tonewheel', 'amplifier']
+        components: ['keys', 'drawbars', 'pedals', 'tonewheel', 'amplifier'],
+        photo: './assets/b3.jpeg'
     },
     {
         name: 'Fender Jazzmaster',
         family: 'guitar',
-        components: ['body', 'neck', 'bridge', 'frets', 'strings', 'pickups']    
+        components: ['body', 'neck', 'bridge', 'frets', 'strings', 'pickups'],
+        photo: './assets/jazzmaster.jpeg'
     },
     {
         name: 'Tenor Saxophone',
         family: 'woodwind',
-        components: ['mouthpiece', 'neck', 'body', 'bow', 'bell']
+        components: ['mouthpiece', 'neck', 'body', 'bow', 'bell'],
+        photo: './assets/saxophone.jpeg'
     }
 ];
