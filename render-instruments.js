c24f9b814388e5cd6ef5ce26582c30d48222692d
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-07-29 18:39:06 -0500

    Create renderInstrument function, copy/paste hard-coded html and comment it out

---
+++ render-instruments.js
@@ -0,0 +1,21 @@
+function renderInstrument(instrument) {
+    //         <h3>Instruments:</h3>
+    //         <div class="instrument-item">
+    //             <p>Hammond B3 Organ</p>
+    //             <img src="assets/b3.jpeg">
+    //             <p>Keyboard</p>
+    //             <p>keys, drawbars, pedals, tonewheel, amplifier</p>
+    //         </div>
+    //         <div class="instrument-item">
+    //             <p>Fender Jazzmaster</p>
+    //             <img src="assets/jazzmaster.jpeg">
+    //             <p>Guitar</p>
+    //             <p>body, neck, bridge, frets, strings, pickups</p>
+    //         </div>
+    //         <div class="instrument-item">
+    //             <p>Tenor Saxophone</p>
+    //             <img src="assets/saxophone.jpeg">
+    //             <p>Woodwind</p>
+    //             <p>mouthpiece, neck, body, bow, bell</p>
+    //         </div>
+}
