dfea426fc5879c45ef4473bdbbcb3813c09dfd51
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-07-29 17:25:18 -0500

    Create instruments array and rename file

---
+++ instruments-data.js
@@ -0,0 +1,17 @@
+const instruments = [
+    {
+        name: 'Hammond B3 Organ',
+        family: 'keyboard',
+        components: ['keys', 'drawbars', 'pedals', 'tonewheel', 'amplifier']
+    },
+    {
+        name: 'Fender Jazzmaster',
+        family: 'guitar',
+        components: ['body', 'neck', 'bridge', 'frets', 'strings', 'pickups']    
+    },
+    {
+        name: 'Tenor Saxophone',
+        family: 'woodwind',
+        components: ['mouthpiece', 'neck', 'body', 'bow', 'bell']
+    }
+];
