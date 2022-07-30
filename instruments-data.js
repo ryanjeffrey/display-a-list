21193d9b3dc25591ec36d0d45dab144a74c1e0d2
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-07-29 19:06:36 -0500

    Reorder properties to match intended render order

--- instruments-data.js
+++ instruments-data.js
@@ -1,20 +1,20 @@
 const instruments = [
     {
         name: 'Hammond B3 Organ',
+        photo: './assets/b3.jpeg',
         family: 'keyboard',
         components: ['keys', 'drawbars', 'pedals', 'tonewheel', 'amplifier'],
-        photo: './assets/b3.jpeg'
     },
     {
         name: 'Fender Jazzmaster',
+        photo: './assets/jazzmaster.jpeg',
         family: 'guitar',
         components: ['body', 'neck', 'bridge', 'frets', 'strings', 'pickups'],
-        photo: './assets/jazzmaster.jpeg'
     },
     {
         name: 'Tenor Saxophone',
+        photo: './assets/saxophone.jpeg',
         family: 'woodwind',
         components: ['mouthpiece', 'neck', 'body', 'bow', 'bell'],
-        photo: './assets/saxophone.jpeg'
     }
 ];
