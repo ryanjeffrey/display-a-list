d705b5e69fba620da1c1675ab028c9289bc9f790
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-07-29 20:51:06 -0500

    Create data for chicago sports section

---
+++ chicago-sports-data.js
@@ -0,0 +1,32 @@
+export const chicagoSports = [
+    {
+        team: 'Bears',
+        stadium: 'Soldier Field',
+        yearEstablished: '1920',
+        starPlayer: {
+            name: 'Justin Fields',
+            number: '1',
+            age: '23'
+        }
+    },
+    {
+        team: 'Bulls',
+        stadium: 'United Center',
+        yearEstablished: '1966',
+        starPlayer: {
+            name: 'Zach LaVine',
+            number: '8',
+            age: '27'
+        }
+    },
+    {
+        team: 'White Sox',
+        stadium: 'Guaranteed Rate Field',
+        yearEstablished: '1900',
+        starPlayer: {
+            name: 'José Abreu',
+            number: '79',
+            age: '35'
+        }
+    }
+];
