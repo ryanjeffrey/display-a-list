6561f1ed692386a60bf9d507d8183228ebdc4909
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-07-29 19:47:33 -0500

    Grab instruments section from the DOM

--- app.js
+++ app.js
@@ -2,11 +2,5 @@
 import { instruments } from './instruments-data.js';
 import { renderInstrument } from './render-instruments.js';
 
-console.log(instruments, renderInstrument);
-
-// initialize global state
-
-// set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+const instrumentsSection = document.getElementById('instruments-section');
+console.log(instrumentsSection);
