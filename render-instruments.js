7f75e0b5e510c2878409d1bc7b33b7ba2e4c9391
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-07-29 20:15:37 -0500

    Remove commented template

--- render-instruments.js
+++ render-instruments.js
@@ -1,17 +1,4 @@
 export function renderInstrument(instrument) {
-    //         <div class="instrument-item">
-    //             <p>Hammond B3 Organ</p>
-    //             <img src="assets/b3.jpeg">
-    //             <p>Keyboard</p>
-    //             <ul>
-    //                  <li>keys</li> 
-    //                  <li>drawbars</li> 
-    //                  <li>pedals</li> 
-    //                  <li>tonewheel</li> 
-    //                  <li>amplifier</li> 
-    //             </ul>
-    //         </div>
-
     const instrumentEl = document.createElement('div');
     const nameEl = document.createElement('p');
     const photoEl = document.createElement('img');
