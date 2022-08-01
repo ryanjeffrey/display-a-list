ecf0e7a337d85e32bb87664f9b603d48282c07c5
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-07-31 21:00:57 -0500

    Create data for meshell albums

---
+++ meshell-albums-data.js
@@ -0,0 +1,20 @@
+export const meshellAlbums = [
+    {
+        title: 'Comfort Woman',
+        year: '2003',
+        photo: '',
+        songs: ['Love Song #1', 'Come Smoke My Herb', 'Andromeda and the Milky Way', 'Love Song #2', 'Body', 'Liliquoi Moon', 'Love Song #3', 'Fellowship', 'Good Intentions', 'Thankful']
+    },
+    {
+        title: 'The World Has Made Me the Man of My Dreams',
+        year: '2007',
+        photo: '',
+        songs: ['Haditha', 'The Sloganeer', 'Evolution', 'Virgo', 'Lovely Lovely', 'Elliptical', 'Shirk', 'Article 3', 'Michelle Johnson', 'Headline', 'Solomon', 'Relief: A Stripper Classic', 'Soul Spaceship']
+    },
+    {
+        title: `Devil's Halo`,
+        year: '2009',
+        photo: '',
+        songs: ['Slaughter', 'Tie One On', 'Lola', 'Hair Of The Dog', 'Mass Transit', 'White Girl', 'Love You Down', `Devil's Halo`, 'Bright Shiny Morning', 'Blood On The Curb', 'Die Young', 'Crying In Your Beer']
+    }
+];
