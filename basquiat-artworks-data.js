7409498ee25f6544781ca0e209728f607783e336
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-07-31 20:09:02 -0500

    Add photos to assets and add photo property to objects

--- basquiat-artworks-data.js
+++ basquiat-artworks-data.js
@@ -1,18 +1,21 @@
 export const basquiatArtworks = [
     {
         title: 'Riding with Death',
+        photo: './assets/riding-with-death.png',
         year: '1988',
         medium: 'acrylic and oil stick on canvas',
         size: '248.9 x 289.5 cm',
     },
     {
         title: 'Hollywood Africans',
+        photo: './assets/hollywood-africans.png',
         year: '1983',
         medium: 'acrylic and oil stick on canvas',
         size: '213.5 x 213.4 cm',
     },
     {
         title: 'Untitled',
+        photo: './assets/untitled.png',
         year: '1981',
         medium: 'acrylic and oil stick on canvas',
         size: '205.74 x 175.9 cm',
