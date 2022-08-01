4e162f057adfa8b6f3c67242390ad2fe16387cc0
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-07-31 20:36:25 -0500

    Import data and render function / loop, render, append

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 // import functions and grab DOM elements
 import { instruments } from './instruments-data.js';
 import { chicagoSports } from './chicago-sports-data.js';
+import { basquiatArtworks } from './basquiat-artworks-data.js';
 import { renderInstrument } from './render-instruments.js';
 import { renderChicagoSport } from './render-chicago-sports.js';
+import { renderBasquiatArtwork } from './render-basquiat-artworks.js';
 
 const instrumentsSection = document.getElementById('instruments-section');
 
@@ -16,4 +18,11 @@ const chicagoSportsSection = document.getElementById('sports-section');
 for (let sport of chicagoSports) {
     const sportEl = renderChicagoSport(sport);
     chicagoSportsSection.append(sportEl);
+}
+
+const basquiatArtworksSection = document.getElementById('basquiat-section');
+
+for (let artwork of basquiatArtworks) {
+    const artworkEl = renderBasquiatArtwork(artwork);
+    basquiatArtworksSection.append(artworkEl);
 }
