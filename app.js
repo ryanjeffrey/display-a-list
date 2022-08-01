f1062c741b8061485809d70b44164521c575d070
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-07-31 21:59:26 -0500

    Reorder lists

--- app.js
+++ app.js
@@ -1,26 +1,14 @@
 // import functions and grab DOM elements
-import { instruments } from './instruments-data.js';
-import { chicagoSports } from './chicago-sports-data.js';
 import { basquiatArtworks } from './basquiat-artworks-data.js';
 import { meshellAlbums } from './meshell-albums-data.js';
-import { renderInstrument } from './render-instruments.js';
-import { renderChicagoSport } from './render-chicago-sports.js';
+import { instruments } from './instruments-data.js';
+import { chicagoSports } from './chicago-sports-data.js';
+
 import { renderBasquiatArtwork } from './render-basquiat-artworks.js';
 import { renderMeshellAlbum } from './render-meshell-albums.js';
+import { renderInstrument } from './render-instruments.js';
+import { renderChicagoSport } from './render-chicago-sports.js';
 
-const instrumentsSection = document.getElementById('instruments-section');
-
-for (let instrument of instruments) {
-    const instrumentEl = renderInstrument(instrument);
-    instrumentsSection.append(instrumentEl);
-}
-
-const chicagoSportsSection = document.getElementById('sports-section');
-
-for (let sport of chicagoSports) {
-    const sportEl = renderChicagoSport(sport);
-    chicagoSportsSection.append(sportEl);
-}
 
 const basquiatArtworksSection = document.getElementById('basquiat-section');
 
@@ -34,4 +22,18 @@ const meshellAlbumsSection = document.getElementById('meshell-section');
 for (let album of meshellAlbums) {
     const albumEl = renderMeshellAlbum(album);
     meshellAlbumsSection.append(albumEl);
+}
+
+const instrumentsSection = document.getElementById('instruments-section');
+
+for (let instrument of instruments) {
+    const instrumentEl = renderInstrument(instrument);
+    instrumentsSection.append(instrumentEl);
+}
+
+const chicagoSportsSection = document.getElementById('sports-section');
+
+for (let sport of chicagoSports) {
+    const sportEl = renderChicagoSport(sport);
+    chicagoSportsSection.append(sportEl);
 }
