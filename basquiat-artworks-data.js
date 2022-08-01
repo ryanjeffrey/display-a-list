6149ff0aca274e1fb0603dc91f5f443b46794de6
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-07-31 20:01:13 -0500

    Add data for basquiat works

---
+++ basquiat-artworks-data.js
@@ -0,0 +1,20 @@
+export const basquiatArtworks = [
+    {
+        title: 'Riding with Death',
+        year: '1988',
+        medium: 'acrylic and oil stick on canvas',
+        size: '248.9 x 289.5 cm',
+    },
+    {
+        title: 'Hollywood Africans',
+        year: '1983',
+        medium: 'acrylic and oil stick on canvas',
+        size: '213.5 x 213.4 cm',
+    },
+    {
+        title: 'Untitled',
+        year: '1981',
+        medium: 'acrylic and oil stick on canvas',
+        size: '205.74 x 175.9 cm',
+    }
+];
