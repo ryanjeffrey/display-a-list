fac218067bb041c343d2a92a4b7f1044dd5973a6
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-07-31 20:38:38 -0500

    Create and export function to render artworks

---
+++ render-basquiat-artworks.js
@@ -0,0 +1,19 @@
+export function renderBasquiatArtwork(artwork) {
+    const artworkEl = document.createElement('div');
+    const titleEl = document.createElement('p');
+    const photoEl = document.createElement('img');
+    const yearEl = document.createElement('p');
+    const mediumEl = document.createElement('p');
+    const sizeEl = document.createElement('p');
+
+    titleEl.textContent = `${artwork.title}, ${artwork.year}`;
+    photoEl.src = artwork.photo;
+    mediumEl.textContent = artwork.medium;
+    sizeEl.textContent = artwork.size;
+
+    artworkEl.append(titleEl, photoEl, yearEl, mediumEl, sizeEl);
+    artworkEl.classList.add('basquiat-item');
+    photoEl.classList.add('artwork-photo');
+
+    return artworkEl;
+}
